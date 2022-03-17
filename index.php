fad8b3cea0576e70f570fa8ddf2ec3076ba7061c
MoenirEl <1186234@student.roc-nijmegen.nl>
2022-03-17 08:45:32 +0100

    meer boxen gemaakt

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,41 +13,74 @@
     <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
 
 </head>
+
 <body>
-    
+
     <div class="Main_Base">
         <div class="Top_Bar">
-            <img class="icon" src="img/fork.png" >
+            <img class="icon" src="img/fork.png">
             <h2>thuisbezorgd.nl </h2>
             <div class="Dropdown">
                 <button class="dropbtn">restaurant</button>
                 <div class="dropdown_content">
-                    <a href="cafetaria.php">cafetaria</a>
-                    <a href="pizzeria.php">pizzaeria</a>
-                    <a href="sushi.php">sushi</a>
-                    <a href="thais.php">thais</a>
-                    <a href="index.php">home</a>
+                    <a href="sushi.php">Sushi</a>
+                    <a href="inloggen.php">Log in</a>
                 </div>
             </div>
-    
-                
-        </div>
-       
 
-            <div class="Mid_Base">
-                <div class="Midden_Base" >
-                    <h1 class="grote_tekst" >Tijd om eten te</h1>
-                    <h1 class="aparte_tekst" > bestellen </h1>
-                    <p class="kleine_tekst" >we hebben nou 4 nieuwe restaurants! </p>
-                    
-                </div>
-            </div>
-            
-       
-            <div class="img"></div>
-            
+
         </div>
-    
-        
-    </body>
-    </html>
+
+
+        <div class="Mid_Base">
+            <div class="Midden_Base">
+                <h1 class="grote_tekst">Tijd om eten te</h1>
+                <h1 class="aparte_tekst"> bestellen </h1>
+                <p class="kleine_tekst">we hebben nou een nieuwe restaurant! </p>
+
+            </div>
+        </div>
+        <div class="img_box">
+            <div class="img">
+
+            </div>
+        </div>
+
+
+        <div class="restaurant">
+            <div class="col-2">
+                <h1 class="grote_tekst2">Log in of register </h1>
+
+                <h2 class="middel_tekst"> voor extra bonusen!</h2>
+
+
+                <ul class="lijst">
+
+                    <li class="space">
+                        Voor de eerste keer 20% korting
+                    </li>
+                    <li>
+                        Als u vaak bestelt op de zelfde account 5% korting
+                    </li>
+                    <li>
+                        We geven aanraders voor langere blijvende klanten
+                    </li>
+
+                </ul>
+
+                <button> </button>
+
+
+            </div>
+            <div class="col-2">
+                <h1>nieuw sushi restaurant</h1>
+
+            </div>
+
+        </div>
+    </div>
+
+
+</body>
+
+</html>
--- sushi.php
+++ sushi.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,24 +8,23 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/main.css">
 </head>
+
 <body>
     <div class="Main_Base">
         <div class="Top_Bar">
-            <img class="icon" src="img/fork.png" >
+            <img class="icon" src="img/fork.png">
             <h2>thuisbezorgd.nl </h2>
             <div class="Dropdown">
                 <button class="dropbtn">restaurant</button>
                 <div class="dropdown_content">
-                  <a href="cafetaria.php">cafetaria</a>
-                  <a href="pizzeria.php">pizzaeria</a>
-                  <a href="sushi.php">sushi</a>
-                  <a href="thais.php">thais</a>
-                  <a href="index.php">home</a>
+                    <a href="index.php">Home</a>
+                    <a href="inloggen.php">Log in</a>
                 </div>
-              </div>
+            </div>
 
         </div>
 
     </div>
 </body>
+
 </html>
