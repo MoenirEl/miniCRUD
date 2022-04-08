2f69971dc8317eab9dc2da9f426c0a84c1fd60d6
Moenir <94520561+MoenirEl@users.noreply.github.com>
2022-04-08 11:02:21 +0200

    menukaart (#2)

--- random.php
+++ random.php
@@ -9,7 +9,7 @@ header (location: index.php);
 foreach ($result as $record)
 {
 ?>
-<h3>titel =  <?php echo $record[]</h3>
+<h3>titel =  <?php echo $record[]?> </h3>
 
 }
 
--- sushi.php
+++ sushi.php
@@ -7,16 +7,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/menu.css">
 </head>
 
 <body>
-<main class="img">
-    <?php
-    include_once 'nav.php';
-    ?>
+    <main class="blur img">
 
-    
-</main>
+        <?php
+        include_once 'nav.php';
+        ?>
+        <div class="containter">
+            <div class="row">
+                <div class="menu_kaarten wok">
+                    <strong>wok</strong>
+                </div>
+            </div>
+            <div class="row">
+                <div class="menu_kaarten shushi">
+                    <strong>shushi</strong>
+                </div>
+            </div>
+            <div class="row">
+                <div class="menu_kaarten dumplings">
+                    <strong>dumplings</strong>
+                </div>
+            </div>
+            <div class="row">
+                <div class="menu_kaarten loempia">
+                    <strong>Loempia</strong>
+                </div>
+            </div>
+            <div class="row">
+                <div class="menu_kaarten pollo">
+                    <strong>pollo en agridulce</strong>
+                </div>
+            </div>
+            <div class="row">
+                <div class="menu_kaarten  pla">
+                    <strong>Pla Muek Yang</strong>
+                </div>
+            </div>
+        </div>
+
+    </main>
 </body>
+
 </html>
